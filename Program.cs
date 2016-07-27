c06606586fdb3fbf27188a78e07c64987d5f566f
smarly.net <smarly@smarly.net>
2016-07-27 18:17:41 +0300

    simple web app + dotnet command line publish

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace IISDeploy
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var host = new WebHostBuilder()
+						.UseKestrel()
+						.UseIISIntegration()
+						.UseStartup<Startup>()
+						.Build();
+
+			host.Run();
+		}
+	}
+
+	public class Startup
+	{
+		public void Configure(IApplicationBuilder app)
+		{
+			app.Run(async (context) =>
+			{
+				await context.Response.WriteAsync(
+						"Hello World. The Time is: " +
+						DateTime.Now.ToString("hh:mm:ss tt"));
+			});
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # asp-net-core-iis-publishing
+
+dotnet restore
+
+dotnet publish "D:\Temp\asp-net-core-iis-publishing" --framework netcoreapp1.0 --output "d:\Temp\2" --configuration Releas
+e
